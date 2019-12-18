e57034b96978c90f1a92df6349699ce8882c406e
Laurie Voss <github@seldo.com>
2019-12-18 15:46:41 -0800

    Home page populated

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const db = require('mariadb')
 
-// You can delete this file if you're not using it
+let posts
+
+const getPosts = async () => {
+
+    const pool = db.createPool({
+        host: process.env.DB_HOST, 
+        user: process.env.DB_USER, 
+        password: process.env.DB_PASS, 
+        database: process.env.DB_NAME,
+        connectionLimit: 5
+    })    
+
+    let conn = await pool.getConnection()
+    let rows = await conn.query("SELECT * from content ORDER BY created DESC LIMIT 10")
+
+    return rows
+
+}
+
+// fetches new posts before pages are generated
+exports.onPreBootstrap = async () => {
+    posts = await getPosts()
+}
+
+// recreates each page node with additional context
+exports.onCreatePage = ({ page, actions }) => {
+    const { createPage, deletePage } = actions
+    deletePage(page)
+    createPage({
+        ...page,
+        context: {
+            ...page.context,
+            posts
+        },
+    })
+}
