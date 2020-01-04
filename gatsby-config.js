939ae8e71e7c296c8cea145588981f75ce44f1e1
Laurie Voss <github@seldo.com>
2020-01-04 10:25:28 -0800

    Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,17 @@ module.exports = {
     author: `@seldo`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-1536085-2",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },    
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
