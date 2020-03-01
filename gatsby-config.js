bd35f89b94a1c7e22545ecc955a31b39529391b1
Laurie Voss <github@seldo.com>
2020-03-01 14:08:23 -0800

    Whose blog is it?

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,7 +103,7 @@ module.exports = {
               }
             `,
             output: "/rss.xml",
-            title: "Your Site's RSS Feed",
+            title: "Seldo.com RSS Feed",
           },
         ],
       },
